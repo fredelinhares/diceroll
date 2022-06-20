9e9f3234852c33e6b810dd7abd57effd378c31c2
Frederyk Linhares <Frederyk.Linhares-ext@ab-Inbev.com>
2022-06-20 00:36:23 -0300

    add behavior to mainfragment

--- diceroller/diceroll/src/main/java/com/example/diceroll/MainFragment.kt
+++ diceroller/diceroll/src/main/java/com/example/diceroll/MainFragment.kt
@@ -1,10 +1,13 @@
 package com.example.diceroll
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import kotlin.random.Random
 
 
 class MainFragment : Fragment() {
@@ -13,4 +16,12 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val button = view.findViewById<Button>(R.id.rollDiceButton)
+        val result = view.findViewById<TextView>(R.id.rollResultTextView)
+        button.setOnClickListener {
+            val randomNumber: Int = Random.Default.nextInt(6)
+            result.text = "Result: " + randomNumber
+        }
+    }
 }
