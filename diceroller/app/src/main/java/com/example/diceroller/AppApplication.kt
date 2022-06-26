72e3fbb66967fb716afd1cfba5ae4d34cdebf508
Frederyk Linhares <Frederyk.Linhares-ext@ab-Inbev.com>
2022-06-26 19:42:23 -0300

    Start a Koin Application as StandAlone. Add AppApplication ref in manifest file.

--- diceroller/app/src/main/java/com/example/diceroller/AppApplication.kt
+++ diceroller/app/src/main/java/com/example/diceroller/AppApplication.kt
@@ -1,14 +1,19 @@
 package com.example.diceroller
 
 import android.app.Application
+import com.example.diceroller.di.AppModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class AppApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin{
-
+        startKoin {
+            androidContext(this@AppApplication)
+            modules(
+                AppModule.getModules()
+            )
         }
     }
 }
