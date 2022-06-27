14d8e23fbfd277d8eb97e88b183afcad5fd35c4f
Frederyk Linhares <Frederyk.Linhares-ext@ab-Inbev.com>
2022-06-27 20:31:36 -0300

    create dummie layout to recyclerview. add empty list fragment still without adapter.

---
+++ diceroller/diceroll/src/main/java/com/example/diceroll/ui/DiceRollListFragment.kt
@@ -0,0 +1,24 @@
+package com.example.diceroll.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.diceroll.databinding.DicerollListFragmentBinding
+
+class DiceRollListFragment : Fragment() {
+
+    private var _binding: DicerollListFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = DicerollListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
