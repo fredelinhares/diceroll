8457e2093fa6f359eac433ffa00ec8172312baaa
Frederyk Linhares <Frederyk.Linhares-ext@ab-Inbev.com>
2022-06-30 20:01:47 -0300

    add DiceRollListViewModel to koin module

--- diceroller/diceroll/src/main/java/com/example/diceroll/di/UIModule.kt
+++ diceroller/diceroll/src/main/java/com/example/diceroll/di/UIModule.kt
@@ -1,9 +1,11 @@
 package com.example.diceroll.di
 
 import com.example.diceroll.MainViewModel
+import com.example.diceroll.ui.dicerollList.DiceRollListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 internal val uiModule = module {
     viewModel { MainViewModel() }
+    viewModel { DiceRollListViewModel() }
 }
